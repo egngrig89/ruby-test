c0e0335deeeadca0541c9c429b8450684db87dde
egngrig89 <egngrig@gmail.com>
2026-04-14 19:03:10 +0000

    Final

--- app/models/dresscode.rb
+++ app/models/dresscode.rb
@@ -1,10 +1,10 @@
 class Dresscode
-  # Ruby konstanty (pole řetězců)
+  
   TOPS = ["vytahaný triko", "mikina s kapucí", "merch z konference", "košile (jen pro Zoom s klientem)"]
   BOTTOMS = ["pohodlný tepláky", "kraťasy", "jeansy", "pyžamový kalhoty (homeoffice style)"]
 
   def self.generate
-    # Metoda .sample vybere náhodný prvek - čistá Ruby krása
+    
     "Dneska nahoď #{TOPS.sample} a k tomu #{BOTTOMS.sample}."
   end
 end
